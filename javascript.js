0f908859853edfa30f6d01774105b58ce988d214
Tiffany Ray <tiffanyraylee@gmail.com>
2019-02-01 08:48:48 -0600

    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+
+  var color = ['#160F29', '#28464B', '#326771', '#2C8C99', '#42D9C8'];
+
+  function randomColor() {
+    return color[Math.floor(Math.random() * color.length)];
+  }
+
+  $('.box').on('mouseenter', function () {
+    $(this).css('backgroundColor', randomColor());
+    $(this).css('box-shadow', "0 0 8px white");
+  });
+
+  $('.box').on('mouseleave', function () {
+    $(this).css('box-shadow', "none");
+  });
+
+
+
+
+
+
+
+
+  // End of jquery .ready function
+});
